cb715568161c2e5d4903b90c75acadba45e57c12
umesh-2407 <146861342+umesh-2407@users.noreply.github.com>
2024-08-01 13:35:27 +0530

    Lec - 3

---
+++ Conditionals_Loops.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+   int a,b;
+   cout << "Enter No. : ";
+   cin >> a ;
+
+   cout << "Enter No. : ";
+   cin >> b ;
+ 
+
+   if(a>b){
+    cout << a <<endl;
+   }
+
+   else{
+    cout << b <<endl;
+   }
+
+    return 0;
+}
