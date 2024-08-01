f6517d5635841f2b9b5917d766a34cca7355bb69
umesh-2407 <146861342+umesh-2407@users.noreply.github.com>
2024-08-01 13:05:41 +0530

    Lec-2

---
+++ DataTypes.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int a = 10;
+    // cout << a << endl ;
+
+    // char b = 'b';
+    // cout << b << endl ;
+
+    // float f = 10.098;
+    // cout << f << endl ;
+
+    // double d = 10.098;
+    // cout << d << endl ;
+
+    // bool m = 1;
+    // cout << m << endl ;
+
+    cout << sizeof(a) << endl;
+
+    return 0;
+}
